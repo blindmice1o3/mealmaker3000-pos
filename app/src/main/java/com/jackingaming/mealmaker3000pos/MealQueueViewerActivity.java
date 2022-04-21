ded4be38e0591ade01f5c11719c2875106445270
jamesgov <blindmice1o3@gmail.com>
2022-04-20 19:01:15 -0700

    tinker: Post and Get List<MenuItem> to MealMaker3000WebService
    Next, instead of storing MenuItemAsJSONString in the kafka topic, store MealAsJSONString.

--- app/src/main/java/com/jackingaming/mealmaker3000pos/MealQueueViewerActivity.java
+++ app/src/main/java/com/jackingaming/mealmaker3000pos/MealQueueViewerActivity.java
@@ -2,15 +2,58 @@ package com.jackingaming.mealmaker3000pos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.jackingaming.mealmaker3000pos.models.menuitems.Bread;
+import com.jackingaming.mealmaker3000pos.models.menuitems.MenuItem;
+import com.jackingaming.mealmaker3000pos.models.menuitems.Water;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MealQueueViewerActivity extends AppCompatActivity {
+    private final String URL_GET_AS_JSON_ARRAY = "http://192.168.1.143:8080/kafka/receive_jsonarray";
+    private static final String CONTENT_OF_STRING_BUILDER = "sb";
+
     private TextView textViewMealQueueViewer;
     private Button buttonRefresh;
+
+    private StringBuilder sb = new StringBuilder();
+
+    private static final String PREFERENCE_CONTENT_OF_SB = "preferenceContentOfSB";
+
+    public static boolean setPreference(Context context, String key, String value) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_CONTENT_OF_SB, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(key, value);
+        return editor.commit();
+    }
+
+    public static String getPreference(Context context, String key) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_CONTENT_OF_SB, Context.MODE_PRIVATE);
+        return settings.getString(key, "defaultValue");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setPreference(getApplicationContext(), "keySB", sb.toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +71,73 @@ public class MealQueueViewerActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT)
                         .show();
 
-                
+                getDataAsJSONArray();
             }
         });
+
+        String stringFromPreviousSB = getPreference(getApplicationContext(), "keySB");
+        if (stringFromPreviousSB.equals("defaultValue")) {
+            Toast.makeText(getApplicationContext(), "nothing saved in preferences", Toast.LENGTH_SHORT).show();
+        } else {
+            sb.append(stringFromPreviousSB);
+        }
+    }
+
+    private void getDataAsJSONArray() {
+        Toast.makeText(this, "getDataAsJSONArray() called", Toast.LENGTH_SHORT).show();
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
+                URL_GET_AS_JSON_ARRAY,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.i("jsonArrayRequest::", response.toString());
+
+                        if (response.length() != 0) {
+                            List<MenuItem> menuItems = new ArrayList<MenuItem>();
+                            try {
+                                for (int i = 0; i < response.length(); i++) {
+                                    JSONObject jsonObject = response.getJSONObject(i);
+                                    String name = jsonObject.getString("name");
+
+                                    if (name.equals("bread")) {
+                                        Bread bread = new Bread(jsonObject);
+                                        menuItems.add(bread);
+                                    } else if (name.equals("water")) {
+                                        Water water = new Water(jsonObject);
+                                        menuItems.add(water);
+                                    } else {
+                                        Log.i("MainActivity", ".getDataAsJSONArray(): Not bread, not water");
+                                    }
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                            for (MenuItem menuItem : menuItems) {
+                                sb.append(menuItem.getName() + "\n");
+                            }
+
+//                            String previousMenuItemsOnTextView = textViewMealQueueViewer.getText().toString();
+//                            String newMenuItemsToAppendToTextView = sb.toString();
+                            textViewMealQueueViewer.setText(
+//                                    previousMenuItemsOnTextView + newMenuItemsToAppendToTextView
+                                    sb.toString()
+                            );
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    "response.length() == 0", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("jsonArrayRequest::", "onErrorResponse(VolleyError)");
+                    }
+                });
+
+        AppController.getInstance(this).addToRequestQueue(jsonArrayRequest);
     }
 }
--- app/src/main/java/com/jackingaming/mealmaker3000pos/MealStagingActivity.java
+++ app/src/main/java/com/jackingaming/mealmaker3000pos/MealStagingActivity.java
@@ -1,21 +1,36 @@
 package com.jackingaming.mealmaker3000pos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.jackingaming.mealmaker3000pos.models.menuitems.Bread;
 import com.jackingaming.mealmaker3000pos.models.menuitems.MenuItem;
 import com.jackingaming.mealmaker3000pos.models.menuitems.Water;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MealStagingActivity extends AppCompatActivity {
+    private final String URL_POST_AS_JSON_ARRAY = "http://192.168.1.143:8080/kafka/publish_jsonarray";
+
     private TextView textViewMealViewer;
+    private Button buttonPostMealToKafka;
+
     private Button buttonBread;
     private Button buttonWater;
 
@@ -29,8 +44,19 @@ public class MealStagingActivity extends AppCompatActivity {
         menuItems = new ArrayList<MenuItem>();
 
         textViewMealViewer = findViewById(R.id.textView_meal_viewer);
+        buttonPostMealToKafka = findViewById(R.id.button_post_meal_to_kafka);
         buttonBread = findViewById(R.id.button_bread);
         buttonWater = findViewById(R.id.button_water);
+
+        buttonPostMealToKafka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: double check this functionality.
+                postDataAsJSONArray(menuItems);
+                menuItems.clear();
+                updateTextView();
+            }
+        });
 
         buttonBread.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,7 +75,29 @@ public class MealStagingActivity extends AppCompatActivity {
         });
     }
 
-    public void updateTextView() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu_meal_staging_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull android.view.MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_change_to_queue_viewer:
+                Toast.makeText(getApplicationContext(),
+                        "queue viewer options menu item selected.",
+                        Toast.LENGTH_SHORT).show();
+                Intent intentMealQueueViewer = new Intent(this, MealQueueViewerActivity.class);
+                startActivity(intentMealQueueViewer);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void updateTextView() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < menuItems.size(); i++) {
             String nameOfMenuItem = menuItems.get(i).getName();
@@ -58,5 +106,28 @@ public class MealStagingActivity extends AppCompatActivity {
         }
 
         textViewMealViewer.setText(sb.toString());
+    }
+
+    private void postDataAsJSONArray(List<MenuItem> menuItems) {
+        JSONArray jsonArray = new JSONArray();
+        for (MenuItem menuItem : menuItems) {
+            jsonArray.put(menuItem.toJSON());
+        }
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST,
+                URL_POST_AS_JSON_ARRAY,
+                jsonArray,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        AppController.getInstance(this).addToRequestQueue(jsonArrayRequest);
     }
 }
